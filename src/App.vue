088b5f709a5a7cc1042496402c89edb05d2d93d4
Sonny Brown <lowlifearcade@gmail.com>
2022-04-08 08:57:51 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <Nav />
+  <div class="container">
+    <div class="flex-grid-thing">
+      <Nav />
+      <h1>The Daily Snail</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,8 +15,21 @@ export default {
 </script>
 <style>
 #app {
+  /* variables */
+  --page-gutter: 10px;
+  --num-grid-columns: 10;
+
   font-family: Lato, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  line-height: 1.15;
+}
+.container {
+  position: relative;
+}
+.flex-grid-thing {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,59 @@
 <template>
-  <nav>navbar</nav>
+  <div class="grid-frame-global">
+    <header>
+      <nav>
+        <a href="/" class="home">
+          <h2>The Daily Snail</h2>
+        </a>
+        <ul>
+          <li class="nav-bar-item">link</li>
+          <li class="nav-bar-item">link</li>
+          <li class="nav-bar-item">link</li>
+          <li class="nav-bar-item">link</li>
+          <li class="nav-bar-item">link</li>
+        </ul>
+        <div class="user-menu">
+          <div class="profile">Profile</div>
+          <div class="gear">Gear</div>
+        </div>
+      </nav>
+    </header>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped lang="scss">
+.grid-frame-global {
+  z-index: 9998;
+  width: 100%;
+  max-width: 1280px;
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-gap: 0 20px;
+  background-color: pink;
+  padding: 0 var(--page-gutter);
+}
+header {
+  grid-column: span var(--num-grid-columns);
+}
 nav {
-  background: sandybrown;
+  width: 100%;
+  height: 80px;
+  /* background: sandybrown; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ul {
+    display: flex;
+    list-style: none;
+    background: blue;
+  }
+}
+.user-menu {
+  background-color: aqua;
 }
 </style>
