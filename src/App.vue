1c84e44281ba0bfefe7d2ddd05cdece9225eb9cf
Sonny Brown <lowlifearcade@gmail.com>
2022-04-08 12:34:11 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,10 @@
   --num-grid-columns: 10;
 
   font-family: Lato, Helvetica, Arial, sans-serif;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  
   line-height: 1.15;
 }
 </style>
