876e954deccdf93cc559e4d92307c7b2a81c9762
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2022-04-07 21:50:51 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <Nav />
 </template>
 
+<script>
+import Nav from './components/Nav.vue';
+export default {
+  components: { Nav },
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Lato, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>navbar</nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+nav {
+  background: sandybrown;
+}
+</style>
